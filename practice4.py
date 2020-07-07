b7d20e173fba7048bffb93756b592c9f639fb6f0
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-07 18:29:37 -0500

    conditional statement practice4

---
+++ practice4.py
@@ -0,0 +1,19 @@
+#
+# Example file for working with conditional statements
+#
+
+
+def main():
+    x, y = 10, 100
+
+
+    # conditional flow uses if, elif, else
+    if (x < y):
+        st = "x is less than y"
+    
+    print (st)
+
+    # conditional statements let you use "a if C else b"
+
+if __name__ == main "__main__":
+    main()
