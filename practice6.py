cce3f0a6ef3cc06f2d71cb26482577ec087ba9bf
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-07 21:15:01 -0500

    more code for classes

--- practice6.py
+++ practice6.py
@@ -8,10 +8,27 @@ class myClass():
 
     def method2(self, someString):
         print("myClass method2  + someString")
+
+
+class anotherClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print("anotherClass method1")
+
+    def method2(self, someString):
+        print("anotherClass method2  + someString")
+
+
 def main():
     c = myClass()
     c.method1()
-    
+    c.method2("this is a string ")
+
+    c2= anotherClass()
+    c2.method1()
+
+
+
 if __name__ == "__main__":
     main()
 
