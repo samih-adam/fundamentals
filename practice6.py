1aa0880ba8d40aea0e1e6163d3ced25a349c4d9a
Samih Abdeljabbar <abdels6375@gmail.com>
2020-07-07 21:03:51 -0500

    working with classes now

---
+++ practice6.py
@@ -0,0 +1,15 @@
+#
+# Example file for working with classes 
+#
+
+class myClass():
+    def method1(self):
+        print("myClass method1")
+
+    def method2(self, someString):
+        print("myClass method2  + someString")
+def main():
+
+if __name__ == "__main__":
+    main()
+
